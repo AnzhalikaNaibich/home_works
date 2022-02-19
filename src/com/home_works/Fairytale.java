24025c3ab9ceda8b98ad785f3503c84a0484279e
AnzhalikaNaibich <ANGElika.1991@yandex.ru>
2022-02-19 18:44:56 +0300

    Home work #4 (Skazka)

---
+++ src/com/home_works/Fairytale.java
@@ -0,0 +1,72 @@
+package com.home_works;
+
+import com.home_works.classes.Animal;
+import com.home_works.classes.Human;
+
+public class Fairytale {
+    public static void main(String args[]){
+        Human grandfather = new Human("Starik", "male", 85);
+        Human grandmother = new Human("Staruha","female", 85);
+        Animal fish = new Animal("Fish");
+        fish.say(1);
+        System.out.println();
+        grandfather.say(1, grandfather.getName());
+        System.out.println();
+        grandfather.say(2, grandfather.getName());
+        System.out.println();
+        grandmother.say(3, grandmother.getName());
+        System.out.println();
+        fish.say(2);
+        System.out.println();
+        grandfather.say(4, grandfather.getName());
+        System.out.println();
+        fish.say(3);
+        System.out.println();
+        grandmother.say(5, grandmother.getName());
+        System.out.println();
+        fish.say(2);
+        System.out.println();
+        grandfather.say(6, grandfather.getName());
+        System.out.println();
+        fish.say(4);
+        System.out.println();
+        grandmother.say(7, grandmother.getName());
+        System.out.println();
+        fish.say(2);
+        System.out.println();
+        grandfather.say(8, grandfather.getName());
+        System.out.println();
+        fish.say(5);
+        System.out.println();
+        grandfather.say(9, grandfather.getName());
+        System.out.println();
+        grandmother.say(10, grandmother.getName());
+        System.out.println();
+        grandfather.say(11, grandfather.getName());
+        System.out.println();
+        grandmother.say(12, grandmother.getName());
+        System.out.println();
+        fish.say(2);
+        System.out.println();
+        grandfather.say(13, grandfather.getName());
+        System.out.println();
+        fish.say(6);
+        System.out.println();
+        grandfather.say(14, grandfather.getName());
+        System.out.println();
+        grandmother.say(15, grandmother.getName());
+        System.out.println();
+        fish.say(2);
+        System.out.println();
+        grandfather.say(16, grandfather.getName());
+        System.out.println();
+        System.out.println("Ничего не сказала рыбка,\n" +
+                "Лишь хвостом по воде плеснула\n" +
+                "И ушла в глубокое море.\n" +
+                "Долго у моря ждал он ответа,\n" +
+                "Не дождался, к старухе воротился —\n" +
+                "Глядь: опять перед ним землянка;\n" +
+                "На пороге сидит его старуха,\n" +
+                "А пред нею разбитое корыто.");
+    }
+}
---
+++ src/com/home_works/classes/Animal.java
@@ -0,0 +1,40 @@
+package com.home_works.classes;
+
+public  class Animal {
+
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void say(int j){
+         switch (j) {
+             case 1:
+                 System.out.println(name + ": " + "Отпусти ты, старче, меня в море,\n" +
+                         "Дорогой за себя дам откуп:\n" +
+                         "Откуплюсь чем только пожелаешь");
+                 break;
+             case 2:
+                 System.out.println(name + ": " + "Чего тебе надобно, старче?");
+                 break;
+             case 3:
+                 System.out.println(name + ": " + "Не печалься, ступай себе с богом,\n" +
+                         "Будет вам новое корыто");
+                 break;
+             case 4:
+                 System.out.println(name + ": " + "Не печалься, ступай себе с богом,\n" +
+                         "Так и быть: изба вам уж будет");
+                 break;
+             case 5:
+                 System.out.println(name + ": " + "Не печалься, ступай себе с богом");
+                 break;
+             case 6:
+                 System.out.println(name + ": " + "Не печалься, ступай себе с богом!\n" +
+                         "Добро! будет старуха царицей!");
+                 break;
+
+         }
+
+     }
+}
