09834ac3e7f9f3cd2c9fe6deb81c64aa26d512aa
AnzhalikaNaibich <ANGElika.1991@yandex.ru>
2022-02-22 13:17:18 +0300

    Home work #4 (zadachka)

---
+++ src/com/home_works/HomeWork_4/HomeWork_4.java
@@ -0,0 +1,13 @@
+package com.home_works.HomeWork_4;
+
+public class HomeWork_4 {
+    public int One(int a, int b){
+        int c = a / b;
+        System.out.println(c);
+        return c;
+    }
+
+    public void Two(String name, String size, String old){
+        System.out.println(name + " " + size + " " + old);
+    }
+}
---
+++ src/com/home_works/HomeWork_4/Main.java
@@ -0,0 +1,10 @@
+package com.home_works.HomeWork_4;
+public class Main {
+    public static void main(String[] args) {
+
+        HomeWork_4 HomeWork = new HomeWork_4();
+        HomeWork.One(18, 3);
+        HomeWork.Two("BMW", "sedan", "5");
+    }
+
+}
